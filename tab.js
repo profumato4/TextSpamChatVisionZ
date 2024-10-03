e11c384fc7c92c321ed3a437265772c2e12c55e1
Lenny <Leonardobelli2006@gmail.com>
2024-10-03 18:25:42 +0200

    Update tab.js

--- tab.js
+++ tab.js
@@ -1,22 +1,21 @@
-function openTab(spamVersion){
-    console.log("ff");
+function openTab(spamVersion) {
     var i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
+      tabcontent[i].style.display = "none";
     }
     tablinks = document.getElementsByClassName("tablinks");
     for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
     document.getElementById(spamVersion).style.display = "block";
-}
-
-
-document.getElementById('spamv1').addEventListener('click', () => {
+  }
+  
+  document.getElementById('spamv1').addEventListener('click', () => {
     openTab('v1');
-});
-
-document.getElementById('spamv2').addEventListener('click', ()=>{
+  });
+  
+  document.getElementById('spamv2').addEventListener('click', () => {
     openTab('v2');
-});
+  });
+  
